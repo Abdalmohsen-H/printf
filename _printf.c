0da37e7b1d960de4c20c03591c0f85eb87647e43
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-23 00:16:14 -0700

    refactoring

--- _printf.c
+++ _printf.c
@@ -3,24 +3,17 @@
 #include <string.h>
 #include "main.h"
 /**
-* _printf - simulate printf task 0
+* _printwformat - rint with format
 * @format: character string (zero or more directives)
-*
+* @idx: arg index
+* @count: printed char counter
+* @args: variable argument list
 * Return: num. of printed characters
 * (excluding null byte '\0' used at end of strings)
 */
-int _printf(const char *format, ...)
-{int idx = 0, count = 0;
-va_list args;  /* Declare variable arguments list */
-char c;
+int _printwformat(const char *format, int idx, int count, va_list args)
+{char c;
 char *str;/*, *valid_specifiers = "cs%";*/
-va_start(args, format); /*Initialize variable arguments*/
-if (!format || (format[0] == '%' && format[1] == '\0'))
-	return (-1);
-for (; format && format[idx]; idx++) /*loop format chars and print*/
-{
-if (format[idx] == '%')  /*if it's a format specifier */
-{idx++;  /* increment to next character after '%' */
 switch (format[idx])  /* get format specifier then act*/
 {
 case 'c':  /* Character specifier */
@@ -42,6 +35,27 @@ count += write(1, "%", 1);  /* Write '%' chara to standard output */
 va_end(args);
 return (-1);
 }
+return (count);
+}
+/**
+* _printf - simulate printf task 0
+* @format: character string (zero or more directives)
+*
+* Return: num. of printed characters
+* (excluding null byte '\0' used at end of strings)
+*/
+int _printf(const char *format, ...)
+{int idx = 0, count = 0;
+va_list args;  /* Declare variable arguments list */
+
+va_start(args, format); /*Initialize variable arguments*/
+if (!format || (format[0] == '%' && format[1] == '\0'))
+	return (-1);
+for (; format && format[idx]; idx++) /*loop format chars and print*/
+{
+if (format[idx] == '%')  /*if it's a format specifier */
+{idx++;  /* increment to next character after '%' */
+count = _printwformat(format, idx, count, args);
 }
 else  /*if it's a character that doesn't have % before it*/
 {count += write(1, &format[idx], 1);  /* Write character to standard output*/
--- main.h
+++ main.h
@@ -3,5 +3,6 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+int _printwformat(const char *format, int idx, int count, va_list args);
 
 #endif
