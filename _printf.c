d30a3b918868d5e8021360931be1bc0baf4452b5
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-22 22:14:39 -0700

    task 0 fix invalid specifier check before print

--- _printf.c
+++ _printf.c
@@ -9,17 +9,23 @@
 * (excluding null byte '\0' used at end of strings)
 */
 int _printf(const char *format, ...)
-{
-int idx = 0, count = 0;
+{int idx = 0, count = 0;
 va_list args;  /* Declare variable arguments list */
 char c;
-char *str;
+char *str, *valid_specifiers = "cs%";
+
 va_start(args, format); /*Initialize variable arguments*/
-while (format && format[idx])  /* Loop via each character in format*/
-{
-if (format[idx] == '%')  /*if it's a format specifier */
-{
-idx++;  /* increment to next character after '%' */
+if (!format || (format[0] == '%' && format[1] == '\0'))
+	return (-1);
+for (; format && format[idx] && format[idx] == '%'; idx++)
+{/*check if there invalid specifier */
+if (strchr(valid_specifiers, format[idx + 1]) == NULL)
+{va_end(args);
+return (-1);/* Invalid specifier */
+}
+}
+for (; format && format[idx] && format[idx] == '%'; idx++)
+{idx++;  /* increment to next character after '%' */
 switch (format[idx])  /* get format specifier then act*/
 {
 case 'c':  /* Character specifier */
@@ -34,15 +40,10 @@ case '%':  /* '%' specifier */
 va_arg(args, int);
 count += write(1, "%", 1); /*Write char to standard output*/
 break;
-default:  /* Invalid specifier */
-write(1, "%", 1);  /* Write '%' chara to standard output */
-return (-1);
-}
 }
 else  /*if it's a character that doesn't have % before it*/
 {count += write(1, &format[idx], 1);  /* Write character to standard output*/
 }
-idx++;  /*Move to next character in format*/
 }
 va_end(args);  /*End variable arguments*/
 return (count);  /*Return number of characters printed*/
