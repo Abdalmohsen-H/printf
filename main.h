2fef81b01de265d19344214bcb061ad0fe6f20e1
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-24 10:59:04 +0000

    update

--- main.h
+++ main.h
@@ -1,8 +1,13 @@
 #ifndef printf_header
 #define printf_header
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
 int _printf(const char *format, ...);
 int _putchar(char c);
 int _printwformat(const char *format, int idx, int count, va_list args);
 int _printnum(va_list args);
+
 #endif
