e58eddf9560046cbbab501f65fce996017fb0f2c
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-23 21:37:20 -0700

    task 0

--- main.h
+++ main.h
@@ -1,6 +1,6 @@
 #ifndef printf_header
 #define printf_header
-
+#include <stdarg.h>
 int _printf(const char *format, ...);
 int _putchar(char c);
 int _printwformat(const char *format, int idx, int count, va_list args);
